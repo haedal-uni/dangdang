886bc17f0cdaf26f7dafe11dd65d3144e504e415
manijang2 <manijang2@naver.com>
2021-11-22 21:57:24 +0900

    프로젝트 초기 세팅 및 .gitingnore 파일 설정

---
+++ src/main/java/com/sparta/dangdang/DangDangApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.dangdang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangDangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DangDangApplication.class, args);
+    }
+
+}
