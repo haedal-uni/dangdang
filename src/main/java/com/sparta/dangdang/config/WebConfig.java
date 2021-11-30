6a54aa38d5bab1cb0cc608510af80eac5907166a
manijang2 <manijang2@naver.com>
2021-11-30 18:46:27 +0900

    [update] CORS 규칙중 PUT, DELETE 메소드 추가 [#2]

--- src/main/java/com/sparta/dangdang/config/WebConfig.java
+++ src/main/java/com/sparta/dangdang/config/WebConfig.java
@@ -10,6 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST");
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
