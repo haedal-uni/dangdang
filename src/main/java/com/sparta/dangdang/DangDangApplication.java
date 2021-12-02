d3991eda6ab3b86b9a4f830d37eca083bd38d21a
Lluroa <luora@naver.com>
2021-12-02 19:32:12 +0900

    [add] s3 연동코드 작성(연결 확인x) [#30]

--- src/main/java/com/sparta/dangdang/DangDangApplication.java
+++ src/main/java/com/sparta/dangdang/DangDangApplication.java
@@ -2,14 +2,26 @@ package com.sparta.dangdang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class DangDangApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DangDangApplication.class, args);
-    }
+//    public static void main(String[] args) {
+//        SpringApplication.run(DangDangApplication.class, args);
+//    }
+//
+//}
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "classpath:aws.yml";
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(DangDangApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
+    }
 }
--- src/main/java/com/sparta/dangdang/controller/UploadController.java
+++ src/main/java/com/sparta/dangdang/controller/UploadController.java
@@ -2,13 +2,13 @@ package com.sparta.dangdang.controller;
 
 import com.sparta.dangdang.domain.Upload;
 import com.sparta.dangdang.dto.UploadDto;
+import com.sparta.dangdang.service.S3Uploader;
 import com.sparta.dangdang.service.UploadService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 
 @RequiredArgsConstructor
@@ -20,11 +20,16 @@ public class UploadController {
     public Upload setUpload(UploadDto uploadDto){
         return uploadService.setUpload(uploadDto);
     }
+
+
+
+    private final S3Uploader s3Uploader;
+    @PostMapping("/images")
+    public String upload(@RequestParam("images") MultipartFile multipartFile) throws IOException {
+        s3Uploader.upload(multipartFile, "static");
+        return "test";
+    }
+
 }
-//    @PostMapping("/api/upload/image"){
-//        public Upload setPhoto(@RequestBody PhotoDto photoDto {
-//            return uploadService.setPhoto(photoDto);
-//        }
-    //}
-    //@GetMapping("/api/upload")
+
 
