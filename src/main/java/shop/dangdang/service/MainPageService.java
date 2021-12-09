5277cbb1605ee868b82327860e81ed703935d3ce
Yu_Jehyeop <fbwpguq@gmail.com>
2021-12-10 04:54:14 +0900

    [add] mainpage 백엔드파일추가 [#none]

---
+++ src/main/java/shop/dangdang/service/MainPageService.java
@@ -0,0 +1,31 @@
+package shop.dangdang.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import shop.dangdang.domain.Membership;
+import shop.dangdang.domain.Registry;
+import shop.dangdang.dto.MainPageDto;
+import shop.dangdang.repository.MembershipRepository;
+import shop.dangdang.repository.RegistryRepository;
+
+@RequiredArgsConstructor
+@Service
+public class MainPageService {
+    private final RegistryRepository registryRepository;
+    private final MembershipRepository membershipRepository;
+
+    // 사진 정보 main 페이지로 가져오기(전체)
+    public MainPageDto getMainInfo(Long registryIdx) {
+        Registry registries = registryRepository.findById(registryIdx).orElseThrow(
+                () -> new NullPointerException("해당 게시글이 없습니다.")
+        );
+
+        Membership memberships = membershipRepository.findById(registryIdx).orElseThrow(
+                () -> new NullPointerException("해당 게시글이 없습니다.")
+        );
+
+        MainPageDto mainPageDto = new MainPageDto(registries.getImage(), registries.getContent(), memberships.getNickName());
+
+        return mainPageDto;
+    }
+}
