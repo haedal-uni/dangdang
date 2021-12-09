aeb6af268f147177ade5595257cc125aa90b4e79
manijang2 <manijang2@naver.com>
2021-12-10 04:33:49 +0900

    [add] 레지스트리 등록시 피드도 같이 등록되도록 설정 [#73]

--- src/main/java/shop/dangdang/service/RegistryService.java
+++ src/main/java/shop/dangdang/service/RegistryService.java
@@ -2,12 +2,17 @@ package shop.dangdang.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import shop.dangdang.domain.Feed;
 import shop.dangdang.domain.Membership;
 import shop.dangdang.domain.Registry;
+import shop.dangdang.dto.FeedDetailResponseDto;
 import shop.dangdang.dto.MembershipDto;
 import shop.dangdang.dto.RegistryDto;
+import shop.dangdang.repository.FeedRepository;
 import shop.dangdang.repository.MembershipRepository;
 import shop.dangdang.repository.RegistryRepository;
+import shop.dangdang.util.SecurityUtil;
+
 import javax.transaction.Transactional;
 import java.io.IOException;
 import java.util.List;
@@ -18,12 +23,20 @@ public class RegistryService {
     private final S3Uploader s3Uploader;
     private final RegistryRepository registryRepository;
     private final MembershipRepository membershipRepository;
+    private final FeedRepository feedRepository;
 
     @Transactional
     public Registry setUpload(RegistryDto uploadDto) throws IOException{
         String image = s3Uploader.upload(uploadDto.getImage(),"dang");
         Registry upload = new Registry(uploadDto,image);
         registryRepository.save(upload);
+
+        // 선만 추가
+        String nickName = SecurityUtil.getCurrentUsername().get();
+        Membership writer = membershipRepository.findByNickName(nickName).orElseThrow(NullPointerException::new);
+        Feed feed = new Feed(uploadDto, image, writer);
+        feedRepository.save(feed);
+
         return upload;
     }
 
@@ -43,7 +56,7 @@ public class RegistryService {
 
     //회원가입할 때 정보 가져오기
     public Membership getMembership(MembershipDto membershipDto) {
-        return membershipRepository.findByNickname(membershipDto.getNickname()).orElseThrow(
+        return membershipRepository.findByNickName(membershipDto.getNickName()).orElseThrow(
                 () -> new NullPointerException("해당 닉네임이 존재하지 않습니다.")
         );
     }
