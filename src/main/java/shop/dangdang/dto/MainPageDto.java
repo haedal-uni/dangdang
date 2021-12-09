7c343a599c12f5f543c14a856940c512a4fe81f8
Yu_Jehyeop <fbwpguq@gmail.com>
2021-12-10 04:05:46 +0900

    [기능 추가] mainpage 사진,글 가져오기 [#67]

---
+++ src/main/java/shop/dangdang/dto/MainPageDto.java
@@ -0,0 +1,22 @@
+package shop.dangdang.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MainPageDto {
+    private String address;
+    private String content;
+    private String puppy;
+    private MultipartFile image;
+    private String nickName;
+    private MultipartFile profileimage;
+
+    public MainPageDto(String image, String content, String nickName, String profileImgUrl) {
+    }
+
+}
